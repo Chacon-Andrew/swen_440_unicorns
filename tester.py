7bf81904b83bd2b9d42640d8a79a5849dfa1af24
Chacon-Andrew <chaconandrew226@gmail.com>
2023-04-05 08:44:23 -0400

    Update tester.py

--- tester.py
+++ tester.py
@@ -1,14 +1,15 @@
 import http.server
 import socketserver
+import requests
+import time
 
 PORT = 8000
 
 Handler = http.server.SimpleHTTPRequestHandler
 
 def test_performance():
-    with socketserver.TCPServer(("", PORT), Handler) as httpd:
-        print("Serving at port", PORT)
-        httpd.serve_forever()
+    response_API = requests.post('https://seappserver2.rit.edu/api/ProcessFile?ocrlib=std')
+    print(response_API.status_code)
 
 def test_interoperability():
     with socketserver.TCPServer(("", PORT), Handler) as httpd:
@@ -19,3 +20,12 @@ def test_utilization():
     with socketserver.TCPServer(("", PORT), Handler) as httpd:
         print("Serving at port", PORT)
         httpd.serve_forever()
+
+def main():
+    start = time.time()
+    test_performance()
+    end = time.time()
+    print(end - start)
+
+if __name__=="__main__":
+    main()
